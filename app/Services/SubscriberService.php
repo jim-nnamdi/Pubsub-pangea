0a5050b540a0da9b7c39aac7571a4aec0a4deed0
jim-nnamdi <jimsamuel50@gmail.com>
2021-12-12 07:48:54 +0100

    + set up the subscriber service class

--- app/Services/SubscriberService.php
+++ app/Services/SubscriberService.php
@@ -1,7 +1,33 @@
 <?php 
 
 namespace App\Services; 
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 class SubscriberService {
-  
+
+  public function subscribeToPublishedTopics($topic) {
+
+    $connection = new AMQPStreamConnection(
+      config('RabbitMQ.server'), 
+      config('RabbitMQ.server_port'), 
+      config('RabbitMQ.server_name'), 
+      config('RabbitMQ.server_pwd')
+    );
+
+    $channel = $connection->channel();
+
+    $channel->queue_declare($topic, false, false, false, false); 
+
+    echo '[x] Please wait for incoming messages, press CTRL + C to exit';
+
+    $callback = function($msg){
+      echo '[x]'. $msg->body . 'received';
+    };
+
+    $channel->basic_consume($topic, '', false,true, false, false, $callback );
+
+    while(count($channel->callbacks)){
+      $channel->wait();
+    }
+  }
 }
